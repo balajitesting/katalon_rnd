c8c1eaa0bc14c959800135601a41411e30424ae9
Grace Xu <gxu@guardanthealth.com>
2018-08-29 16:29:38 -0700

    Commit test framework core modules:
    Environment Profiles, PDFCompare, TestUitl, Properties file, JSHandler.

---
+++ Object Repository/Portal/button_SIGN IN.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_SIGN IN</name>
+   <tag></tag>
+   <elementGuidId>76f3ce06-df43-4ef9-b0cc-782a443ad457</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn__size-normal btn__color-blue buttons-row__right show-spinner</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>SIGN IN</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main-content&quot;)/div[@class=&quot;page__row page__row--expanded php-landing-pages&quot;]/div[@class=&quot;container container--breathable php-landing-pages-inner&quot;]/div[@class=&quot;php_landing_container&quot;]/div[@class=&quot;php_landing__grey-box_container&quot;]/form[@class=&quot;fancy-form&quot;]/div[@class=&quot;input-group&quot;]/div[@class=&quot;buttons-row buttons-row__two-with-padding&quot;]/button[@class=&quot;btn__size-normal btn__color-blue buttons-row__right show-spinner&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
