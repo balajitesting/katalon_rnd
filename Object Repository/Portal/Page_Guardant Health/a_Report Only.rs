f10f0b70d592c8edd375fb4d9ac1bf1512632c7f
Grace Xu <gxu@guardanthealth.com>
2018-08-29 16:13:09 -0700

    Commit Objects in Portal OR.

---
+++ Object Repository/Portal/Page_Guardant Health/a_Report Only.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Report Only</name>
+   <tag></tag>
+   <elementGuidId>b3383f58-691d-40bc-83a6-3cd7846be947</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;menu-A80196&quot;)/li[@class=&quot;request__data--download-link no-spinner&quot;]/a[1][count(. | //a[(text() = 'Report Only' or . = 'Report Only')]) = count(//a[(text() = 'Report Only' or . = 'Report Only')])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/reports/98571125?revisions=98571125</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Report Only</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menu-A80196&quot;)/li[@class=&quot;request__data--download-link no-spinner&quot;]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Portal/div_View patients by report st.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_View patients by report st</name>
+   <tag></tag>
+   <elementGuidId>79e69dc6-b082-462b-b017-13a4f93d72e1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>patient-report-status</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>View patients by report status:</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main-content&quot;)/div[@class=&quot;page__row page__row--expanded&quot;]/div[@class=&quot;container container--breathable&quot;]/div[@class=&quot;dashboard-container&quot;]/div[@class=&quot;dashboard__widget__patient-status&quot;]/div[@class=&quot;patient-report-status&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
