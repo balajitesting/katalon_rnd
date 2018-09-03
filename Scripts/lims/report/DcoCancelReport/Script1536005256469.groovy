89d9824097490227cf91e7cfb57914996d5d6271
Grace Xu <gxu@guardanthealth.com>
2018-09-03 13:31:25 -0700

    Commit existing test cases.

---
+++ Scripts/lims/report/DcoCancelReport/Script1536005256469.groovy
@@ -0,0 +1,66 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.junit.After as After
+import org.openqa.selenium.chrome.ChromeOptions as ChromeOptions
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.gh.PDFCompare as PDFCompare
+import gh.core.Properties as Properties
+import gh.core.PDFCompare as PDFCompare
+
+
+WebUI.callTestCase(findTestCase('DCO/DCOAllRequest'), [('A_Number') : A_Number, ('ReportStatus') : ReportStatus, ('RunTest') : RunTest], 
+    FailureHandling.STOP_ON_FAILURE)
+
+CustomKeywords.'lims.service.Logon.logon'('CLIAUserReporting', '5Ed5CIkj9UQfaMZXAkDVaQ==')
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/td_Cancel Request'))
+
+WebUI.setText(findTestObject('LIMS/DCO/Reporting/input_searchtext'), A_Number)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/button_OK'))
+
+Thread.sleep(1000)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/div_Run Workflow'))
+
+Thread.sleep(2000)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/div_Generate Cancelled_Report'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/button_OK_report'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_CNV/div_Release Report'))
+
+Thread.sleep(1000)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/button_OK_2'))
+
+String filename = ((A_Number + '_') + ReportStatus) + '_report.pdf'
+
+String file1 = CustomKeywords.'gh.core.Properties.getGetPDFBaseDir'() + filename
+
+String file2 = CustomKeywords.'gh.core.Properties.getGetPDFDownloadDir'() + filename
+
+if (CustomKeywords.'gh.core.PDFCompare.compareAndSave'(file1, file2)) {
+    println('PDF Match!')
+} else {
+    println('Unmatched pdf found!')
+}
+
+WebUI.closeBrowser()
---
+++ Scripts/lims/report/DcoCreateReportAndReview/Script1536005246382.groovy
@@ -0,0 +1,104 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+//String A_Number = 'A80196'
+
+
+CustomKeywords.'com.gh.lims.Common.logon'('CLIAUserReporting', '5xx1bkCcAlw=')
+
+CustomKeywords.'com.gh.lims.Report.searchRequest'(A_Number)
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_All Requests/select_TB Review BIP DataLD Re'), 60)
+
+WebUI.selectOptionByValue(findTestObject('LIMS/DCO/Reporting/Page_All Requests/select_TB Review BIP DataLD Re'),
+	'LD Review BIP Data', true)
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_All Requests/td_Save'), 60)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_All Requests/td_Save'))
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_All Requests/a_LD Review'), 60)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_All Requests/a_LD Review'))
+
+WebUI.acceptAlert()
+
+WebUI.setText(findTestObject('LIMS/DCO/Reporting/Page_Request List for LD Review/input_searchtext'), A_Number)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Request List for LD Review/td_OK'))
+
+CustomKeywords.'com.gh.lims.Report.clickBeginWorkflowLdReview'()
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_CNV/div_SNV Review'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_SNV/td_Fusion Review'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_INDEL/td_Final Review'))
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/select_FINALAMENDEDCORRECTEDAD'), 180)
+
+WebUI.selectOptionByValue(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/select_FINALAMENDEDCORRECTEDAD'),
+	'AMENDED', true)
+
+WebUI.verifyOptionSelectedByValue(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/select_FINALAMENDEDCORRECTEDAD'), 'AMENDED', true,
+	30)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/td_Generate Report'))
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/button_OK_gen_rep'), 120)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/button_OK_gen_rep'))
+
+Thread.sleep(3000)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_All Requests/a_CLS Review'))
+
+WebUI.setText(findTestObject('LIMS/DCO/Reporting/Page_Request List for CLS Review/input_searchtext'), A_Number)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Request List for CLS Review/td_OK'))
+
+CustomKeywords.'com.gh.lims.Report.clickBeginWorkflowClsReview'()
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_CNV/div_SNV Review'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Request List for CLS Review/td_Fusion Review'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Request List for CLS Review/td_Final Review'))
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/td_Release Report'))
+
+WebUI.waitForElementVisible(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/button_OK_rel_rep'), 120)
+
+WebUI.click(findTestObject('LIMS/DCO/Reporting/Page_Edit GHReportInfo/button_OK_rel_rep'))
+
+WebUI.closeBrowser()
+
+Thread.sleep(2000)
+
+String filename = ((A_Number + '_') + ReportStatus) + '_report.pdf'
+
+String file1 = CustomKeywords.'com.gh.core.Properties.getGetPDFBaseDir'() + filename
+
+String file2 = CustomKeywords.'com.gh.core.Properties.getGetPDFDownloadDir'() + filename
+
+boolean val = CustomKeywords.'com.gh.core.PDFCompare.compareAndSave'(file1, file2)
+
+CustomKeywords.'com.gh.core.PDFCompare.display'(val)
