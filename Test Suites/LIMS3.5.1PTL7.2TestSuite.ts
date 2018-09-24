83413a6f26fe2324e5159c386b22c1b7e4649495
Grace Xu <gxu@guardanthealth.com>
2018-09-24 16:15:38 -0700

    Update test suite to add newly developed tests.

---
+++ Test Suites/LIMS3.5.1PTL7.2TestSuite.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LIMS3.5.1PTL7.2TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-14T13:45:19</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c93d4558-0733-43bc-84c3-6a160c2c4312</testSuiteGuid>
+   <testCaseLink>
+      <guid>58f26970-fb53-41cb-8049-bbc36ce1d83d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/database/LimsDBHealthCheckTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>adb45284-818c-4143-b438-e4d11d9b0a3c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/LimsLoginLogout</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>464a22df-8440-4c45-bd92-949311bf6362</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/accession/SecondDataEntryTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3e608199-9fbf-404d-a794-824f0e41dd98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/report/CreateCancelReportTest</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c3faaa90-b133-43a1-bc66-acf3ad646d9a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/CancelReportTestData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c3faaa90-b133-43a1-bc66-acf3ad646d9a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>A_Number</value>
+         <variableId>33db85da-8811-4a0c-86df-1d8eacda06fb</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c3faaa90-b133-43a1-bc66-acf3ad646d9a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>ReportStatus</value>
+         <variableId>1d006669-4ef5-41c5-aeca-5c0bfb44c42a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e32723f4-a825-48ad-af2c-44b6311ea8b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/report/CreatedAmendReportLdClsReviewtTest</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>88da6d7d-c39b-435f-b4d5-b9405e990317</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/dataCreateReportViewOnPortal</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>88da6d7d-c39b-435f-b4d5-b9405e990317</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>A_Number</value>
+         <variableId>084cfa0f-318e-4599-babd-6c836cca70e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>88da6d7d-c39b-435f-b4d5-b9405e990317</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>ReportStatus</value>
+         <variableId>bc3554ef-ea3f-429d-8835-ebdde22725f2</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a6fa534-8e0a-4dba-9e1f-2184406ac238</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/postsequence/UploadBIPDataBaseTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa9ca232-7839-49b9-9b97-e393f0a1b6ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/request/SearchRequestCancelReportTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>02f2a9c7-9d0d-4bc2-a325-8b273ade84ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/accession/DE2UpdateProblemCaseTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d63641a-d651-4417-8d5c-50b9fdee1956</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/accession/AcessionClinicalDataEntryTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2904683c-84a0-4d0e-bc3d-f9dde69e0559</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/accession/DE1CreateProblemCaseTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b9a4ede-e311-4f70-8237-ebeb403117d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/lims/accession/ResolveProblemCaseTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b6ad5af-87fa-4c22-930e-f2dadf2a21de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/portal/PortalLoginLogout</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8dec99d5-5f26-40f5-ad44-d23bc4a0a8f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/portal/DownloadReportAndViewInPortal</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
