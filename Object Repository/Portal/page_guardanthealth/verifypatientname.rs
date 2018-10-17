786e82c64d9591d115b86830d781d0063d7cd944
Venkata Srinivas Rao <vmamillapelli@guardanthealth.com>
2018-10-17 17:54:11 +0530

    Added TC-3247

---
+++ Object Repository/Portal/page_guardanthealth/verifypatientname.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>verifypatientname</name>
+   <tag></tag>
+   <elementGuidId>1e02bb56-7367-4e56-a54d-489169c7d9c0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class='patient-name__toggle']//div)[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
