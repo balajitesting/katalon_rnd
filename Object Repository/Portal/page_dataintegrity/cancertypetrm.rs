5269019bf5231b832dd0dd1b6f413ad5d712f36c
venkat <vmamillapelli@guardanthealth.com>
2018-11-09 18:46:08 +0530

    Added Ticket-6887

---
+++ Object Repository/Portal/page_dataintegrity/cancertypetrm.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>cancertypetrm</name>
+   <tag></tag>
+   <elementGuidId>b774eab3-56da-49af-a5b9-e3060fe55e0c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='row']//child::div[text()='DIAGNOSIS']//following-sibling::div</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Portal/page_dataintegrity/reportreleasedatedashboard.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>reportreleasedatedashboard</name>
+   <tag></tag>
+   <elementGuidId>9b45959a-a26b-4485-b94b-8628316120fd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class='request__data request__data--report-status'])[1]
+</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
