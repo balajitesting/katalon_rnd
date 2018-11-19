537925a6317163222e725e7e186373b9126acebd
sooryakirankumarrenukunta <sooryakirankumarrenukunta@guardanthealth.com>
2018-11-19 19:29:37 +0530

    Added Ticket-6886

---
+++ Object Repository/LIMS/DCO/Report/Page_All Requests/physicianemail.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>physicianemail</name>
+   <tag></tag>
+   <elementGuidId>85737d7f-c19e-48c3-9e5a-7b91c6861749</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='pr0_email']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/LIMS/DCO/Report/Page_All Requests/iframe_Your browser does not s</value>
+   </webElementProperties>
+</WebElementEntity>
