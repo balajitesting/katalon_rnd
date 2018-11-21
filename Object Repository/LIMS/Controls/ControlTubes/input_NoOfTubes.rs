b70eeb0ff3e302705e94ce4d2af4b00fdc77479b
bantonygh <bantony@guardanthealth.com>
2018-11-21 14:48:47 -0800

    Created the test case - 'Create Aliquot Tubes'

---
+++ Object Repository/LIMS/Controls/ControlTubes/input_NoOfTubes.rs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_NoOfTubes</name>
+   <tag></tag>
+   <elementGuidId>72a81790-3921-4a2a-abf0-09285bfe8833</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='prompt_arg3']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;GenCancelReport&quot;)/tbody[1]/tr[1]/td[@class=&quot;buttonmargin_thin&quot;]/table[1]/tbody[1]/tr[2]/td[1]/div[1][count(. | //*[(text() = 'Run Workflow' or . = 'Run Workflow')]) = count(//*[(text() = 'Run Workflow' or . = 'Run Workflow')])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Run Workflow</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;GenCancelReport&quot;)/tbody[1]/tr[1]/td[@class=&quot;buttonmargin_thin&quot;]/table[1]/tbody[1]/tr[2]/td[1]/div[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/LIMS/Controls/ControlTubes/trmStop_ControlTubes.rs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>trmStop_ControlTubes</name>
+   <tag></tag>
+   <elementGuidId>f7cf03e8-366a-41ce-aa3e-9d09b74ee751</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='ghsdirgtlot_node']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;GenCancelReport&quot;)/tbody[1]/tr[1]/td[@class=&quot;buttonmargin_thin&quot;]/table[1]/tbody[1]/tr[2]/td[1]/div[1][count(. | //*[(text() = 'Run Workflow' or . = 'Run Workflow')]) = count(//*[(text() = 'Run Workflow' or . = 'Run Workflow')])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Run Workflow</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;GenCancelReport&quot;)/tbody[1]/tr[1]/td[@class=&quot;buttonmargin_thin&quot;]/table[1]/tbody[1]/tr[2]/td[1]/div[1]</value>
+   </webElementProperties>
+</WebElementEntity>
