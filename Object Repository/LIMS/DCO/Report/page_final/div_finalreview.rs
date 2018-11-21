2aac51d314e750611e8054f5a7472de71e207768
sooryakirankumarrenukunta <sooryakirankumarrenukunta@guardanthealth.com>
2018-11-21 20:31:36 +0530

    Updated Ticket-6886

---
+++ Object Repository/LIMS/DCO/Report/page_final/div_finalreview.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_finalreview</name>
+   <tag></tag>
+   <elementGuidId>e27e06cc-96a2-4d9f-b28b-01bd55755efb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[text()=''Final Review]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
