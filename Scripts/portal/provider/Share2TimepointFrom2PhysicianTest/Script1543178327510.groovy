d3e5ef6ae6ca40462f51a87c6c4ea8af92eb2e80
Grace Xu <gxu@guardanthealth.com>
2018-11-25 23:59:15 -0800

    Commit new test Share2TimepointFrom2PhysicianTest

---
+++ Scripts/portal/provider/Share2TimepointFrom2PhysicianTest/Script1543178327510.groovy
@@ -0,0 +1,28 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.comment('Run: ENTSW-TC-2921')
+
+CustomKeywords.'com.gh.portal.Common.logon'('skopetz@mdanderson.org', 'R9dwWsVuqf0RB1p2unfSZQ==')
+
+CustomKeywords.'com.gh.portal.Provider.shareReportToPhysician'('2205526', 'kimberly', 'schlesinger', 'kimberly.schlesinger@rivhs.com')
+
+CustomKeywords.'com.gh.portal.Common.logon'('kimberly.schlesinger@rivhs.com', 'R9dwWsVuqf0RB1p2unfSZQ==')
+
+CustomKeywords.'com.gh.portal.Provider.verifyTrm'('2205526', 'A92332')
+
+CustomKeywords.'com.gh.portal.Common.logout'()
+
+WebUI.closeBrowser()
