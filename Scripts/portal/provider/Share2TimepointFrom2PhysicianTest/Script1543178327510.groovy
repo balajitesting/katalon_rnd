3d72eebdf57d6569674242c47e9c1ba4d870e230
Grace Xu <gxu@guardanthealth.com>
2018-11-26 16:50:26 -0800

    Used webdriver to click the 2nd timepoint Share2TimepointFrom2Physicain.

--- Scripts/portal/provider/Share2TimepointFrom2PhysicianTest/Script1543178327510.groovy
+++ Scripts/portal/provider/Share2TimepointFrom2PhysicianTest/Script1543178327510.groovy
@@ -13,6 +13,16 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+import org.junit.After
+import org.openqa.selenium.By as By
+import org.openqa.selenium.JavascriptExecutor
+import com.kms.katalon.core.webui.common.WebUiCommonHelper
+
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+
+
 WebUI.comment('Run: ENTSW-TC-2921')
 
 CustomKeywords.'com.gh.portal.Common.logon'('skopetz@mdanderson.org', 'R9dwWsVuqf0RB1p2unfSZQ==')
@@ -22,6 +32,19 @@ CustomKeywords.'com.gh.portal.Provider.shareReportToPhysician'('2205526', 'kimbe
 CustomKeywords.'com.gh.portal.Common.logon'('kimberly.schlesinger@rivhs.com', 'R9dwWsVuqf0RB1p2unfSZQ==')
 
 CustomKeywords.'com.gh.portal.Provider.verifyTrm'('2205526', 'A92332')
+
+WebDriver driver = DriverFactory.getWebDriver();
+
+String date = 'SEP-29-2017'
+
+List<WebElement> elements = driver.findElements(By.cssSelector("text.tumor-response-map__x-label-text"));
+for (WebElement ele: elements) {         
+    if(ele.getText().equals(date)){
+         ele.click();
+     }
+}
+
+CustomKeywords.'com.gh.portal.Provider.verifyTrm2'('2205526', 'A73393')
 
 CustomKeywords.'com.gh.portal.Common.logout'()
 
