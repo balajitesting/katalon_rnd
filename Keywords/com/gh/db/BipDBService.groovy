705196b4c22b789b8bc247990edb50e3e4ece749
gxuh <39317528+gxuh@users.noreply.github.com>
2018-11-27 11:16:25 -0800

    Update BipDBService.groovy

--- Keywords/com/gh/db/BipDBService.groovy
+++ Keywords/com/gh/db/BipDBService.groovy
@@ -42,17 +42,6 @@ public class BipDBService {
 		return connection
 	}
 
-
-	//	String url = "jdbc:postgresql://localhost/test";
-	//	Properties props = new Properties();
-	//	props.setProperty("user","fred");
-	//	props.setProperty("password","secret");
-	//	props.setProperty("ssl","true");
-	//	Connection conn = DriverManager.getConnection(url, props);
-	//
-	//	String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
-	//	Connection conn = DriverManager.getConnection(url);
-
 	/**
 	 * execute a SQL query on database
 	 * @param queryString SQL query string
